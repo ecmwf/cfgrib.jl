2950a207e3184c7840623a2a2dc7352d01974492
robert.rosca <32569096+RobertRosca@users.noreply.github.com>
2020-01-25 15:41:14 +0100

    Modify script

--- tests/scripts/install-hooks.sh
+++ tests/scripts/install-hooks.sh
@@ -4,7 +4,8 @@ GIT_DIR=$(git rev-parse --git-dir)
 
 echo "Installing hooks..."
 
-ln -s ./pre-push.bash $GIT_DIR/hooks/pre-push
-ln -s ./pre-commit.bash $GIT_DIR/hooks/pre-commit
+ln -s "../../tests/scripts/pre-push.jl" "$GIT_DIR/hooks/pre-push"
 
-echo "Done!
+ln -s "../../tests/scripts/pre-commit.jl" "$GIT_DIR/hooks/pre-commit"
+
+echo "Done!"
