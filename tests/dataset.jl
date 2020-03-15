e09e8d5614a1bae503cbfed0460ff11d67641e07
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-03-16 00:12:10 +0100

    Add extra tests

--- tests/dataset.jl
+++ tests/dataset.jl
@@ -5,16 +5,68 @@ using DataStructures
 
 
 @testset "enforce_unique_attributes" begin
-@test cfgrib.enforce_unique_attributes(
-    OrderedDict("key" => [1]), ["key"]) |> length == 1
-
-for missing_value in [missing, "missing", "undef", "unknown"]
     @test cfgrib.enforce_unique_attributes(
-        OrderedDict("key" => [missing_value]), ["key"]) |> length == 0
+        OrderedDict("key" => [1]), ["key"]) |> length == 1
+
+    for missing_value in [missing, "missing", "undef", "unknown"]
+        @test cfgrib.enforce_unique_attributes(
+            OrderedDict("key" => [missing_value]), ["key"]) |> length == 0
+    end
+
+    @test_throws(
+        cfgrib.DatasetBuildError,
+        cfgrib.enforce_unique_attributes(OrderedDict("key" => [1, 2]), ["key"])
+    )
 end
 
-@test_throws(
-    cfgrib.DatasetBuildError,
-    cfgrib.enforce_unique_attributes(OrderedDict("key" => [1, 2]), ["key"])
-)
+
+@testset "Variable" begin
+    res = cfgrib.Variable(
+        ("lat", ),
+        [1, 2, 3],
+        Dict("Test" => 10)
+    )
+
+    @test res == res
 end
+
+@testset "build_data_var_components_no_encode" begin
+    test_file = joinpath(dir_testfiles, "era5-levels-members.grib")
+    @test isfile(test_file)
+
+    index = cfgrib.FileIndex(
+        test_file,
+        cfgrib.ALL_KEYS
+    )
+
+    @test index["paramId"] == [129, 130]
+
+    cfgrib.filter!(index, paramId=130)
+
+    @test index["paramId"] == [130]
+
+    @test_skip "build_geography_coordinates breaks with paramId 130"
+
+    # dims, data_var, coord_vars = cfgrib.build_variable_components(
+    #     index; log=missing
+    # )
+
+    # @test dims == {'number': 10, 'dataDate': 2, 'dataTime': 2, 'level': 2, 'values': 7320}
+end
+
+# const dir_tests = abspath(joinpath(dirname(pathof(cfgrib)), "..", "tests"))
+# const dir_testfiles = abspath(joinpath(dir_tests, "sample-data"))
+
+# test_file = joinpath(dir_testfiles, "era5-levels-members.grib")
+
+# index = cfgrib.FileIndex(
+#     test_file,
+#     cfgrib.ALL_KEYS
+# )
+
+
+# cfgrib.filter!(index, paramId=130)
+
+# dims, data_var, coord_vars = cfgrib.build_variable_components(
+#     index; log=missing
+# )
--- tests/runtests.jl
+++ tests/runtests.jl
@@ -3,6 +3,10 @@ using Test
 using cfgrib
 
 
+const dir_tests = abspath(joinpath(dirname(pathof(cfgrib)), "..", "tests"))
+const dir_testfiles = abspath(joinpath(dir_tests, "sample-data"))
+
+
 @testset "cfgrib.jl" begin
     @testset "Indexing" begin
         include("indexing.jl")
