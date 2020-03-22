9c0849a8bbaef09402f280ac665d9b8b4c930319
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-03-22 17:02:31 +0100

    build_valid_time fixes and tests

--- tests/cfmessage.jl
+++ tests/cfmessage.jl
@@ -1,6 +1,65 @@
+using Test
+
 using cfgrib
 using GRIB
 
-result = cfgrib.from_grib_date_time(20160706, 1944)
 
-@test result == 1467834240
+@testset "from_grib_date_time" begin
+    result = cfgrib.from_grib_date_time(20160706, 1944)
+
+    @test result == 1467834240
+end
+
+
+@testset "test_build_valid_time" begin
+    forecast_reference_time = [0]
+    forecast_period = [0]
+
+    vt_dims, vt_data = cfgrib.build_valid_time(
+        forecast_reference_time,
+        forecast_period
+    )
+
+    @test vt_dims == ()
+    @test size(vt_data) == ()
+
+
+    forecast_reference_time = [0, 31536000]
+    forecast_period = 0
+
+    vt_dims, vt_data = cfgrib.build_valid_time(
+        forecast_reference_time,
+        forecast_period
+    )
+
+    @test vt_dims == ("time", )
+    @test size(vt_data) == (2, )
+
+
+    forecast_reference_time = 0
+    forecast_period = [0, 12, 24, 36]
+
+    vt_dims, vt_data = cfgrib.build_valid_time(
+        forecast_reference_time,
+        forecast_period
+    )
+
+    @test vt_dims == ("step", )
+    @test size(vt_data) == (4,)
+    # @test np.allclose((data - data[..., :1]) / 3600, forecast_period)
+
+
+    forecast_reference_time = [0, 31536000]
+    forecast_period = [0, 12, 24, 36]
+
+    vt_dims, vt_data = cfgrib.build_valid_time(
+        forecast_reference_time,
+        forecast_period
+    )
+
+    @test vt_dims == ("time", "step")
+    #  TODO: Julia is column major, numpy is row major, not too sure what
+    #  the correct approach would be here...
+    @test size(vt_data) == (4, 2)
+    # @test np.allclose((data - data[..., :1]) / 3600, forecast_period)
+end
