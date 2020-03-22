2936c89e966a762478a67b8db14ebed76786ddc9
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-03-22 15:27:18 +0100

    Multiple dispatch for build valid time

--- src/cfmessage.jl
+++ src/cfmessage.jl
@@ -27,22 +27,45 @@ end
 
 #  TODO: This probably won't work translated directly from python
 #  check cases where time and step are effectively missing
+function build_valid_time(time::Int, step::Int)
+    step_s = step * 3600
+
+    data = time + step_s
+    dims = Tuple{String}(("", ))
+
+    return dims, data
+end
+
 function build_valid_time(time::Array{Int, 1}, step::Int)
     step_s = step * 3600
 
-    if length(size(time)) == 0 && length(size(step)) == 0
-        data = time + step_s
-        dims = Tuple{String}(("", ))
-    elseif length(size(time)) > 0 && length(size(step)) == 0
-        data = time + step_s
-        dims = ("time", )
-    elseif length(size(time)) == 0 && length(size(step)) > 0
-        data = time + step_s
-        dims = ("step", )
-    else
-        data = time .+ step_s
-        dims = ("time", "step")
-    end
+    data = time .+ step_s
+    dims = ("time", )
 
     return dims, data
+end
+
+function build_valid_time(time::Int, step::Array{Int, 1})
+    step_s = step * 3600
+
+    data = time .+ step_s
+    dims = ("step", )
+
+    return dims, data
+end
+
+function build_valid_time(time::Array{Int, 1}, step::Array{Int, 1})
+    step_s = step * 3600
+
+    if size(time) == size(step)
+        data = time + step_s
+        dims = ("time", "step")
+        return dims, data
+    elseif length(unique(step)) == 1
+        return build_valid_time(time, step_s[1])
+    else
+        throw(DimensionMismatch("cannot build valid time, time and step shape"*
+                                " not compatible:" *
+                                " time ($(size(time))), step ($(size(time)))"))
+    end
 end
