72fd3ec5019cd613cc13d3e0db7b24e26cfc8728
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-06-21 14:42:15 +0200

    Update and add FileIO tests

--- test/backends.jl
+++ test/backends.jl
@@ -3,12 +3,13 @@ using InteractiveUtils
 
 using AxisArrays
 using DimensionalData
+using FileIO
 
 test_file = joinpath(dir_testfiles, "era5-levels-members.grib")
 
 BACKENDS = [
-    CfGRIB.Backends.AxisArrayWrapper,
-    CfGRIB.Backends.DimensionalArrayWrapper,
+    CfGRIB.AxisArrayWrapper,
+    CfGRIB.DimensionalArrayWrapper,
 ]
 
 #  Generic tests to see if required methods exist, and that they can at least be
@@ -43,6 +44,20 @@ BACKENDS = [
         @test haskey(da, key)
     end
 end
+
+#  FileIO integration testing
+#  Add GRIB format if it is not already present
+@testset "FileIO integration tests" begin
+    if !(:GRIB in keys(FileIO.sym2info))
+        add_format(format"GRIB", UInt8[0x47, 0x52, 0x49, 0x42], ".grib", [:CfGRIB])
+    end
+
+    @testset "for backend $backend" for backend in BACKENDS
+        @test typeof(load(test_file, backend=backend)) == backend
+    end
+end
+
+
 
 #  Backend specific tests are placed into their own files if required
 include("./backends/dimensionaldata.jl")
